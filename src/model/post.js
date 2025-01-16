960d9d3b4e1be9628e1aabc028c3d268b3cba8cb
esfuerzo <dkdldrha@naver.com>
2025-01-16 13:43:52 +0900

    dummyData #2

---
+++ src/model/post.js
@@ -0,0 +1,15 @@
+export class Post {
+  constructor(title, content) {
+    this.title = title;
+    this.content = content;
+    this.createdAt = new Date();
+  }
+
+  getPostData() {
+    return {
+      title: this.title,
+      content: this.content,
+      createdAt: this.createdAt,
+    };
+  }
+}
