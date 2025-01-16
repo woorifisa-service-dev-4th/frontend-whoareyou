c4a3fc2394f3e6cf887e60ba4939eb7e53a5661e
chans <herryjin@naver.com>
2025-01-16 02:39:38 -0800

    feat: post-detail router

---
+++ src/Router.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PostForm from "./components/posts/PostForm";
+import QuestionBoard from "./components/Questionboard";
+import Header from "./layouts/Header";
+
+const AppRouter = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<QuestionBoard />} />
+      <Route path="/post-detail/:id" element={<PostForm />} />
+    </Routes>
+  </Router>
+);
+
+export default AppRouter;
