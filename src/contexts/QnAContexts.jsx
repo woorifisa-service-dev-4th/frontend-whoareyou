80ce6b8298c1b84dabbb9ef65e45fd6dce29b2b1
chans <herryjin@naver.com>
2025-01-16 02:42:52 -0800

    feat: QnA Contexts

--- src/contexts/QnAContexts.jsx
+++ src/contexts/QnAContexts.jsx
@@ -6,12 +6,35 @@ const QnADispatchContext = createContext();
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'CREATE_POST':
-      return;
-    case 'CREATE_COMMENT':
-      return;
-    case 'CREATE_IMAGE':
-      return;
+    case 'COMMENT_POST':
+      const newComments = [...state.commentData, action.newComment];
+      return {...state, commentData: newComments};
+    case 'RECOMMENT_POST':
+      const newReComments = [...state.commentData, action.newReComment];
+      return {...state, commentData: newReComments};
+    case 'COMMENT_UPDATE':
+      const updateComments = state.commentData.map(comment => comment.id === action.updatedComment.id ? action.updatedComment : comment);
+      return {...state, commentData: updateComments};
+    case 'RECOMMENT_UPDATE' :
+      const updateReComments = state.commentData.map(comment => comment.id === action.updatedComment.id ? action.updatedComment : comment);
+      return {...state, commentData: updateReComments};
+    case 'COMMENT_DELETE':
+      const deleteComments = state.commentData.map(comment => comment.id !== action.deletedComment.id);
+      return {...state, commentData: deleteComments};
+    case 'RECOMMENT_DELETE':
+      const deleteReComments = state.commentData.map(comment => comment.id !== action.deletedComment.id);
+      return {...state, commentData: deleteReComments};
+    case "FILTER":
+      return {
+        ...state,
+        selectedSection: action.selectedSection,
+        selectedCategory: action.selectedCategory,
+      };
+    case "SORT":
+      return {
+        ...state,
+        selectedSort: action.selectedSort,
+      };      
     default:
       return state;
   }
@@ -26,7 +49,7 @@ export const useQnADispatch = () => {
 }
 
 const QnAProvider = ({children}) => {
-  const [state, dispatch] = useReducer(reducer, {postData: postDummyData, commentData: commentDummyData, imageDummyData: imageDummyData});
+  const [state, dispatch] = useReducer(reducer, {postData: postDummyData, commentData: commentDummyData, imageDummyData: imageDummyData, selectedCategory: "ALL", selectedSection: "ALL", selectedSort: "최신순",});
   return (
     <QnAContext.Provider value={state}>
       <QnADispatchContext.Provider value={dispatch}>
