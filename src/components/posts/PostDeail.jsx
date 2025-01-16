cd8841be7a3450c2c3d0fb983885a4dea0a5b8b7
chans <herryjin@naver.com>
2025-01-16 02:41:52 -0800

    feat: pass postid

--- src/components/posts/PostDeail.jsx
+++ src/components/posts/PostDeail.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useQnAContext } from '../../contexts/QnAContexts';
 
-const UserPost = ({ postData }) => {
+const PostDetail = ({postId}) => {
+    const postData = useQnAContext().postData[postId].body;
+
     return (
         <article className="flex flex-col px-20 py-8 w-full text-xl rounded-3xl bg-neutral-100 max-w-[1200px] max-md:px-5 max-md:max-w-full">
             <div className="mt-8 text-2xl leading-9 text-zinc-800 max-md:max-w-full">
@@ -10,4 +13,4 @@ const UserPost = ({ postData }) => {
     );
 };
 
-export default UserPost;
+export default PostDetail;
--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,61 +1,16 @@
 import React from 'react';
-import PostDetails from './PostDeails';
-import CommentSection from '../comments/CommentSection';
-
-// Reducer, Context 사용해야함 변경 요망
-const postData = {
-    id: 0,
-    post: {
-        title: "Test Data 0",
-        content: "This is the first test data"
-    },
-    status: 'active'
-};
-
-const comments = [
-    {
-        id: 0,
-        comment: {
-            post_id: 0,
-            comment_id: 0,
-            content: "This is the first comment"
-        },
-        status: 'active'
-    },
-    {
-        id: 1,
-        comment: (
-            {
-                post_id: 0,
-                comment_id: 1,
-                content: "This is the firt comment's comment"
-            }
-        ),
-        status: 'active'
-    },
-    {
-        id: 2,
-        comment: (
-            {
-                post_id: 0,
-                comment_id: 2,
-                content: "This is the second comment's comment"
-            }
-        ),
-        status: 'active'
-    }
-];
-
+import PostDetail from './PostDeail';
+import Comment from '../comments/Comment';
+import { useParams } from 'react-router-dom';
 
 const PostForm = () => {
+    const {id} = useParams();
+    console.log(id);
     return (
         <main className="flex overflow-hidden flex-col bg-white">
             <div className="flex flex-col self-center mt-24 max-w-full w-[1200px] max-md:mt-10">
-                <PostDetails postData={postData.post} />
-                <CommentSection />
-                {comments.map((comment, index) => (
-                    <PostDetails key={index} postData={comment.comment} />
-                ))}
+                <PostDetail postId={id} />
+                <Comment postId={id} />
             </div>
         </main>
     );
