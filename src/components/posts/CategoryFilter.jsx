1f7f20b8a9fa409c5c4f2d16af1e0a6b98f1dee4
esfuerzo <dkdldrha@naver.com>
2025-01-18 18:53:45 +0900

    수강반,카테고리 filter
    value 및 onChange() 수정 #30

--- src/components/posts/CategoryFilter.jsx
+++ src/components/posts/CategoryFilter.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from 'react'
-import { categoryData } from '../../constants/category'
+import React, { useState } from "react";
+import { categoryData } from "../../constants/category";
 
 const CategoryFilter = ({ onChange }) => {
   return (
     <div className="flex flex-col">
       <div>카테고리</div>
-      <select className="p-2 bg-gray-100 text-gray-800 rounded"
-        onChange={event => onChange(event.target.value)}>
-        <option value={0}>{categoryData[0]}</option>
-        <option value={1}>{categoryData[1]}</option>
-        <option value={2}>{categoryData[2]}</option>
-        <option value={3}>{categoryData[3]}</option>
+      <select
+        className="p-2 bg-gray-100 text-gray-800 rounded"
+        onChange={(event) => onChange(parseInt(event.target.value))}
+      >
+        <option value="0">{categoryData[0]}</option>
+        <option value="1">{categoryData[1]}</option>
+        <option value="2">{categoryData[2]}</option>
+        <option value="3">{categoryData[3]}</option>
       </select>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryFilter
+export default CategoryFilter;
