752363439c3973d52d4ec320be9da7d2feeb1975
esfuerzo <dkdldrha@naver.com>
2025-01-18 18:51:03 +0900

    수강반 정렬 버튼
    + dispatch( type : filter)
    + 버튼 내용 수정 #30

--- src/components/button/SectionButton.jsx
+++ src/components/button/SectionButton.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { useQnAContext, useQnADispatch } from "../../contexts/QnAContexts";
 
 const SectionButton = () => {
+  const postData = useQnAContext();
+  const dispatch = useQnADispatch();
+  const filterTodos = (event) => {
+    dispatch({
+      type: "FILTER",
+      selectedSection: event.target.value,
+      selectedCategory: postData.selectedCategory,
+    });
+  };
+
   return (
     <select
-      //   onChange={changeHandler}
-      //   value={category}
+      value={postData.selectedSection}
+      onChange={filterTodos}
       className="flex gap-3.5 items-center text-sky-600 bg-white"
       //   data-cy="todo-filter"
     >
       <option value="ALL">수강반</option>
-      <option value="TODO"> To do</option>
-      <option value="PROGRESS">On progress</option>
-      <option value="DONE"> Done</option>
+      <option value="0"> 클라우드 서비스 개발</option>
+      <option value="1">클라우드 엔지니어링</option>
+      <option value="2"> AI 엔지니어링</option>
     </select>
   );
 };
